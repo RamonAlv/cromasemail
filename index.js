f0d964de16d4bbe38101b31623aead915f18cf85
Ramon Alvarado <171106@ids.upchiapas.edu.mx>
2020-07-16 20:31:01 -0500

    Ortografía

--- index.js
+++ index.js
@@ -33,17 +33,17 @@ app.post('/api/v2/email',(req,res)=>{//Confirmacion de Cuenta a traves de correo
     Transport.sendMail({
         from: "ESI A.C",
         to: req.body.email,
-        subject: "Hola activa tu cuenta",
+        subject: "Activa tu cuenta",
         html: `
-            <h1>Hola ${req.body.nombre} Bienvenido!</h1>
-            <a href="https://croma.esimx.org/activate/${req.body.token}">Porfavor active su cuenta desde aqui!</a>
+            <h1>Hola, ${req.body.nombre} Bienvenido!</h1>
+            <a href="https://croma.esimx.org/activate/${req.body.token}">Por favor active su cuenta presionando aquí!</a>
         `
     }).then((r) => {
-        res.send({message: r + ' Menseje enaviado'})
+        res.send({message: r + ' Mensaje enviado'})
     }
     )
     .catch((e) => {
-        res.send({message: e + ' Menseje no enaviado'})
+        res.send({message: e + ' Mensaje no enviado'})
     }
     );
 });
@@ -55,15 +55,15 @@ app.post('/api/v2/password',(req,res)=>{//Cambio de contraseña a traves de corr
         to: req.body.email,
         subject: "Solicitud de cambio de contraseña",
         html: `
-            <h1>Hola si usted no hizo la solicitud porfavor ingnore este correo</h1>
+            <h1>Hola, si usted no hizo la solicitud porfavor ignore este correo</h1>
             <a href="https://croma.esimx.org/fpassword/${req.body.token}">Cambio de contraseña aqui!</a>
         `
     }).then((r) => {
-        res.send({message: r + ' Menseje enaviado'})
+        res.send({message: r + ' Mensaje enviado'})
     }
     )
     .catch((e) => {
-        res.send({message: e + ' Menseje no enaviado'})
+        res.send({message: e + ' Mensaje no enviado'})
     }
     );
 });
